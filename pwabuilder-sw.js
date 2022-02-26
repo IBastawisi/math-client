07e8dea0b77c496fd0a4ea527dda4ea27b750dc4
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2022-02-26 22:46:40 +0200

    add print button

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "math-client";
+const CACHE = "math-client v0.0.1";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
